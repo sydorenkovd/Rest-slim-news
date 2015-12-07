acfab6d702f3980bf1ed5337d7e344c16b7636b9
sydorenkovd <sydorenkovd@gmail.com>
2015-12-07 18:35:15 +0200

    reformate code

--- index.php
+++ index.php
@@ -14,92 +14,92 @@ $db = new NotORM($pdo);
 $app = new \Slim\Slim();
 
 /**
-*	Роутинг: определение методов, путей и действий
-*/
+ *    Роутинг: определение методов, путей и действий
+ */
 
 
 /* Действие по-умолчанию */
-$app->get("/", function() {
+$app->get("/", function () {
     echo "Something by default";
 });
 
 /* Выборка всех книг */
-$app->get("/books/", function() use ($app, $db) {
-foreach ($db->books() as $book) {
-	$books[] = [
-	"id" => $book["id"],
-"title" => $book["title"],
-"author" => $book["author"],
-"summary" => $book["summary"]
-	];
-}
-$res = $app->response();
-$res["Content-Type"] = "application/json";
-echo json_encode($books);
+$app->get("/books/", function () use ($app, $db) {
+    foreach ($db->books() as $book) {
+        $books[] = [
+            "id" => $book["id"],
+            "title" => $book["title"],
+            "author" => $book["author"],
+            "summary" => $book["summary"]
+        ];
+    }
+    $res = $app->response();
+    $res["Content-Type"] = "application/json";
+    echo json_encode($books);
 });
 
 /* Получение книги используя её идентификатор */
 $app->get("/book/:id/", function ($id) use ($app, $db) {
-$res = $app->response();
-$res["Content-Type"] = "application/json";
-$book = $db->books()->where("id", $id);
-if ($data = $book->fetch()) {
-	echo json_encode([
-		"id" => $data["id"],
-		"title" => $data["title"],
-		"author" => $data["author"],
-		"summary" => $data["summary"]
-		]);
-} else {
-	echo json_encode([
-		"staus" => 1,
-		"message" => "Book ID $id does not exist"
-		]);
-}
+    $res = $app->response();
+    $res["Content-Type"] = "application/json";
+    $book = $db->books()->where("id", $id);
+    if ($data = $book->fetch()) {
+        echo json_encode([
+            "id" => $data["id"],
+            "title" => $data["title"],
+            "author" => $data["author"],
+            "summary" => $data["summary"]
+        ]);
+    } else {
+        echo json_encode([
+            "status" => 0,
+            "message" => "Book ID $id does not exist"
+        ]);
+    }
 });
 
 /* Добавление новой книги */
-$app->post("/book/", function () use($app, $db) {
-	$res = $app->response();
-	$res["Content-Type"] = "application/json";
-	$book = $app->books->insert($book);
-	echo json_encode(["id" => $result["id"]]);
+$app->post("/book/", function () use ($app, $db) {
+    $res = $app->response();
+    $res["Content-Type"] = "application/json";
+    $book = $app->books->insert($book);
+    echo json_encode(["id" => $result["id"]]);
 });
 
 /* Изменение данных книги используя её идентификатор */
 $app->put("/book/:id/", function ($id) use ($app, $db) {
-	$res = $app->response();
-	$res["Content-Type"] = "application/json";
-	$book = $db->books()->where("id", $id);
-	if ($book->fetch()){
-		$post = $app->request()->put();
-		$result = $book->update($post);
-		echo json_encode([
-			"status" => 1,
-			"message" => "Book updated successfully"
-			]);
-	} else {
-		echo json_encode([
-			"status" => 0,
-			"message" => "Book id $id does not exist"]);
-	}
+    $res = $app->response();
+    $res["Content-Type"] = "application/json";
+    $book = $db->books()->where("id", $id);
+    if ($book->fetch()) {
+        $post = $app->request()->put();
+        $result = $book->update($post);
+        echo json_encode([
+            "status" => 1,
+            "message" => "Book updated successfully"
+        ]);
+    } else {
+        echo json_encode([
+            "status" => 0,
+            "message" => "Book id $id does not exist"]);
+    }
 });
 
 /* Удаление книги используя её идентификатор */
-$app->delete("/book/:id/", function ($id) use($app, $db) {
-	$res = $app->response();
-	$res["Content-Type"] = "application/json";
-	$book = $db->books()->where("id", $id);
-	if ($book->fetch()){
-		$result = $book->delete();
-		echo json_encode([
-			"status" => 1,
-			"message" => "Book deleted successfully"]);
-	} else {
-		echo json_encode([
-			"status" => 0,
-			"message" => "Book id $id does not exist"]);
-	}
+$app->delete("/book/:id/", function ($id) use ($app, $db) {
+    $res = $app->response();
+    $res["Content-Type"] = "application/json";
+    $book = $db->books()->where("id", $id);
+    if ($book->fetch()) {
+        $result = $book->delete();
+        echo json_encode([
+            "status" => 1,
+            "message" => "Book deleted successfully"]);
+    } else {
+        echo json_encode([
+            "status" => 0,
+            "message" => "Book id $id does not exist"]);
+    }
 });
 
 /* Запуск приложения */
